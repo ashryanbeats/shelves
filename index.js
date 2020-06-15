22b3708a03321bcee16e5bcc1abe80bb9427925e
Ash Ryan Arnwine <accounts@ashryan.io>
2020-06-14 21:44:39 -0400

    Added cli.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const wallWidth = 82.5;
-const shelfWidth = 36;
-const shelfCount = 2;
+#!/usr/bin/env node
+const cli = require("cli");
+
+cli.enable("version");
+
+const args = cli.parse({
+  wallWidth: ["w", "Wall width", "int", 82.5],
+  shelfWidth: ["s", "Shelf width", "int", 36],
+  shelfCount: ["c", "Shelf count", "int", 2],
+  bracketPadding: [
+    "p",
+    "Padding between the edge of the shelf and the center of the bracket",
+    "int",
+    5,
+  ],
+});
+
+const { wallWidth, shelfWidth, shelfCount, bracketPadding } = args;
+
+if (shelfWidth * shelfCount > wallWidth) {
+  cli.fatal("Your shelves are wider than your wall.");
+}
 
 const blankSpaceTotal = wallWidth - shelfWidth * shelfCount;
 const blankSpaceCount = shelfCount + 1;
 const singleBlankSpaceWidth = blankSpaceTotal / blankSpaceCount;
-
-const bracketPadding = 6;
-const bracketCount = 4;
 
 const calculateSingleShelfBrackets = (leftMargin) => {
   return {
@@ -17,9 +33,10 @@ const calculateSingleShelfBrackets = (leftMargin) => {
 };
 
 const calculateBracketsForAllShelves = () => {
-  let shelvesWithBrackets = new Array(shelfCount).fill(0).map((shelf, i) => {
+  let shelvesWithBrackets = new Array(shelfCount).fill(0).map((_shelf, i) => {
     const shelfWithMargin = singleBlankSpaceWidth + shelfWidth;
     const leftMargin = singleBlankSpaceWidth + i * shelfWithMargin;
+
     return calculateSingleShelfBrackets(leftMargin);
   });
 
@@ -28,4 +45,4 @@ const calculateBracketsForAllShelves = () => {
 
 const bracketPositions = calculateBracketsForAllShelves();
 
-console.log(bracketPositions);
+cli.output(bracketPositions);
